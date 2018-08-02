001aa7b715950a4cc91d5ff8098f67e96f0a4438
Bernat Gabor <gaborjbernat@gmail.com>
2018-08-02 12:43:22 +0100

    fix

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # janus-release-bot
+
+This bot accomplishes the following release operation:
+
+1. Track a repository for newly created issues.
+2. If an administrator (has merge rights) of the repository
+   creates an issue with "release <commit sha> as <version>"
+   trigger release flow
+3. Create a release commit (e.g. run towncrier - will invoke a tox env for this)
+4. Push and create a PR
+5. Wait for all PR checks to complete with success
+6. Tag the release commit with the version
+7. Do release (call tox env), this is also where you would do release notification.
+8. Merge release PR via a merge commit
+9. Close release issue
+
+For every interaction the logs will be commented onto the release issue. 
--- janus/app.py
+++ janus/app.py
@@ -1,53 +1,14 @@
-import asyncio
-import os
-import sys
-import traceback
+from sanic import Sanic
+from sanic.response import json
 
-import aiohttp
-import cachetools
-from aiohttp import web
-from gidgethub import aiohttp as gh_aiohttp
-from gidgethub import routing, sansio
-
-router = routing.Router()
-cache = cachetools.LRUCache(maxsize=500)
+APP = Sanic()
 
 
-@router.register("pull_request", action="opened")
-async def opened_pr(event, gh, *arg, **kwargs):
-    """Mark new PRs as needing a review."""
-    pull_request = event.data["pull_request"]
-    await gh.post(pull_request["labels_url"], data=["needs review"])
-
-
-async def main(request):
-    try:
-        body = await request.read()
-        secret = os.environ.get("GH_SECRET")
-        event = sansio.Event.from_http(request.headers, body, secret=secret)
-        print("GH delivery ID", event.delivery_id, file=sys.stderr)
-        if event.event == "ping":
-            return web.Response(status=200)
-        oauth_token = os.environ.get("GH_AUTH")
-        async with aiohttp.ClientSession() as session:
-            gh = gh_aiohttp.GitHubAPI(session, "brettcannon/gidgethub-example", oauth_token=oauth_token, cache=cache)
-            # Give GitHub some time to reach internal consistency.
-            await asyncio.sleep(1)
-            await router.dispatch(event, gh)
-        try:
-            print("GH requests remaining:", gh.rate_limit.remaining)
-        except AttributeError:
-            pass
-        return web.Response(status=200)
-    except Exception as exc:
-        traceback.print_exc(file=sys.stderr)
-        return web.Response(status=500)
+@APP.route("/", methods=["POST"])
+async def test(request):
+    msg = request.json
+    return json({"hello": "world"})
 
 
 if __name__ == "__main__":
-    app = web.Application()
-    app.router.add_post("/", main)
-    port = os.environ.get("PORT")
-    if port is not None:
-        port = int(port)
-    web.run_app(app, port=port)
+    APP.run(host="127.0.0.1", port=3000)
